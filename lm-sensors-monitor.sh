b4fd08c119ec8548751c8cd34221a461acc9977f
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-01 17:01:51 -0500

    testing daemonizing with loop

--- lm-sensors-monitor.sh
+++ lm-sensors-monitor.sh
@@ -5,10 +5,15 @@
 maxTemp=15
 pushbulletAPI=P81dRv12Jp7cdza1zCbxyDidnyOl7YSV
 pushbulletDevice=ufenwEfsjAiVsKnSTs
-pushbulletPath=/home/taylor/bin/pyPushBullet
+pushbulletPath="/home/taylor/bin/pyPushBullet"
 #critTemp=
-mmsAddr=8149773797@att.mms.net
+mmsAddr=8149773797@mms.att.net
 emailAddr=taylor.growden@gmail.com
+
+
+# testing daemonizing with a "while" loop to be called at boot
+while [ true ]; do
+
 
 ## ultimately, I would like to use a "for" loop for cpu cores; unfortunately, I am having trouble with the syntax to do that
 
@@ -28,13 +33,17 @@ cpu1=`echo ${arr1[2]} | sed "s/+//"`
 cpu1=`echo ${cpu1} | sed "s/°//"`
 cpu1=`echo ${cpu1} | sed "s/C//"`
 
-
 # test if temps are too high
 # Like I said above, this should be a "for" loop (e.g. "for x in $cpuX"), but I'm having trouble with the syntax
 
-if (( $(echo "$cpu0 $maxTemp" | awk '{print ($1 > $2)}') )); then
+if (( $(echo "$cpu0 $maxTemp" | awk '{print ($1 > $2)}') )) || (( $(echo "$cpu0 $maxTemp" | awk '{print ($1 > $2)}') ) ; then
 # send MMS to my phone via email
-#  echo "CPU0: ${cpu0}C   CPU1: ${cpu1}C" | mail -s "UNSAFE TEMPERATURE!" ${mmsAddr}, ${emailAddr}
+  echo "CPU0: ${cpu0}C   CPU1: ${cpu1}C" | mail -s "UNSAFE TEMPERATURE!" ${mmsAddr},${emailAddr}
 ## Send Pushbullet notification
-  ${pushbulletPath}/pushbullet_cmd.py "${pushbulletAPI}" note "${pushbulletDevice}" "Unsafe Temperature!" "CPU0: ${cpu0}C   CPU1: ${cpu1}C"
+  ${pushbulletPath}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice "Unsafe Temperature!" "CPU0: ${cpu0}C   CPU1: ${cpu1}C"
+# wait 3 minutes so that user isn't bombarded with alerts
+sleep 180
 fi
+
+sleep 5
+done
