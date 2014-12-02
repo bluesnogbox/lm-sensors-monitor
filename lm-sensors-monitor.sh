6a4297eaf89751be90059d46950e45746446fb2e
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-01 21:33:30 -0500

    merge changes from "testing" branch

--- lm-sensors-monitor.sh
+++ lm-sensors-monitor.sh
@@ -10,7 +10,13 @@ pushbulletPath=
 mmsAddr=
 emailAddr=
 
-## ultimately, I would like to use a "for" loop for cpu cores; unfortunately, I am having trouble with the syntax to do that
+
+# testing daemonizing with a "while" loop to be called at boot
+while [ true ]; do
+
+
+## ultimately, I would like to use a "for" loop for setting cpu core variables; I would also like to make the script allow X number of cores, which would allow it to be used on any machine. Unfortunately, I am having trouble with the syntax to do that
+## I also imagine that there is probably a better way to run these sed commands. I certainly welcome input here
 
 # list temp for CPU0
 string0=`sensors |grep CPU0`
@@ -28,12 +34,28 @@ cpu1=`echo ${arr1[2]} | sed "s/+//"`
 cpu1=`echo ${cpu1} | sed "s/°//"`
 cpu1=`echo ${cpu1} | sed "s/C//"`
 
-# test if temps are too high
-# Like I said above, this should be a "for" loop (e.g. "for x in $cpuX"), but I'm having trouble with the syntax
+# Here, I take the readings and send them to a variable, which I then split into an array. I imagine that there is probably a more elegant way to do this.
+read cpu <<<$(echo $cpu0 $cpu1)
+cpu=($cpu)
 
-if (( $(echo "$cpu0 $maxTemp" | awk '{print ($1 > $2)}') )) || (( $(echo "$cpu0 $maxTemp" | awk '{print ($1 > $2)}') ) ; then
-# send MMS to my phone via email
-  echo "CPU0: ${cpu0}C   CPU1: ${cpu1}C" | mail -s "UNSAFE TEMPERATURE!" ${mmsAddr},${emailAddr}
-## Send Pushbullet notification
-  ${pushbulletPath}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice "Unsafe Temperature!" "CPU0: ${cpu0}C   CPU1: ${cpu1}C"
+for x in ${cpu[@]}; do
+if (( $(echo "$x $maxTemp" | awk '{print ($1 > $2)}') )); then
+# Append temperature for $x to /tmp/cpu; this file, and the "notify" variable, will be cleared if a message is sent. This way, this loop allows for X number of cores
+  echo "Core: ${x}C" >> /tmp/cpu
+  cpuFile=`cat /tmp/cpu`
+  notify="yes"
 fi
+done
+if [ $notify == "yes" ]; then
+  echo "$cpuFile" | mail -s "UNSAFE TEMPERATURE!" ${mmsAddr},${emailAddr}
+## Send Pushbullet notification
+  ${pushbulletPath}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice "Unsafe Temperature!" "$cpuFile"
+  rm /tmp/cpu
+  notify=""
+# wait 3 minutes if the temperature is above the "max" level so that user isn't bombarded with alerts. I may also define a "critical" level, which would then scale-back power to core x, but this would require another dependancy
+sleep 180
+fi
+
+# wait 5 seconds between checking core temps
+sleep 5
+done
