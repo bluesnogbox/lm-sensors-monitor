883d66d3f44d685f48cff02c9aded1902abcecc0
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-03 21:49:38 -0500

    added branch based on suggestions made by reddit.com/u/chuzuki

--- lm-sensors-monitor.sh
+++ lm-sensors-monitor.sh
@@ -1,59 +1,43 @@
 #!/bin/bash
 
-# My main concern is how I should daemonize this--I have considered running it as a cron job. I have also thought about using systemd. I'd appreciate any feedback you might have
-
-maxTemp=
+noSpamDelay=180 # Minimum wait in seconds between notifications
+maxTemp=70
 pushbulletAPI=
 pushbulletDevice=
 pushbulletPath=
 #critTemp=
 mmsAddr=
 emailAddr=
-# Enter number of CPU cores here
-coreTotal=2
 
-## ultimately, I would like to use a "for" loop for setting cpu core variables; I would also like to make the script allow X number of cores, which would allow it to be used on any machine. Unfortunately, I am having trouble with the syntax to do that
+# Example sensors -u output:
+# coretemp-isa-0000
+# Adapter: ISA adapter
+# Core 0:
+  # temp2_input: 70.000
+  # temp2_crit: 125.000
+  # temp2_crit_alarm: 0.000
+# Core 1:
+  # temp3_input: 71.000
+  # temp3_crit: 125.000
+  # temp3_crit_alarm: 0.000
 
-# list temp for CPU0
-string0=`sensors |grep CPU0`
-arr0=($string0)
+# Get CPU temps.  sensors -u outputs raw data for easier parsing.
+sensordata=$(sensors -u) # save data so we don't need to run sensors twice
+cputemp0=$(echo "$sensordata" | sed -n 's/[ \t]*temp2_input:[ \t]*//p')
+cputemp1=$(echo "$sensordata" | sed -n 's/[ \t]*temp3_input:[ \t]*//p')
 
-cpu0=`echo ${arr0[2]} | awk '{print substr($0, 2, length($0) - 3)}'`
-
-# list temp for CPU1
-string1=`sensors |grep CPU1`
-arr1=($string1)
-
-cpu1=`echo ${arr1[2]} | awk '{print substr($0, 2, length($0) - 3)}'`
-
-# Here, I take the readings and send them to a variable, which I then split into an array. I imagine that there is probably a more elegant way to do this.
-read cpu <<<$(echo $cpu0 $cpu1)
-cpu=($cpu)
-
-count=0
-for x in ${cpu[@]}; do
-  if (( $(echo "$x $maxTemp" | awk '{print ($1 > $2)}') )); then
-    echo "Core ${count}: ${x}C" >> /tmp/cpu
-    cpuFile=`cat /tmp/cpu`
-    notify="yes"
-    count=$((count + 1))
-  fi
+# Touch cpu temp file to make sure it exists
+touch /tmp/last_cpu_temp_notify
+# Put CPU temps in an array
+cpu=($cputemp0 $cputemp1)
+for x in ${!cpu[@]}; do # ! means loop using the index instead of the value
+    if (( $(echo "${cpu[$x]} $maxTemp" | awk '{print ($1 > $2)}') )); then
+        if (( $(echo "$(date +%s) $(cat /tmp/last_cpu_temp_notify) $noSpamDelay" | awk '{print ($1 - $2 > $3)}') )); then # difference is more than noSpamDelay
+            # Send Pushbullet notification
+            # ${pushbulletPath}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice "UNSAFE TEMPERATURE on Core $x: ${cpu[$x]}"
+            echo "UNSAFE TEMPERATURE on Core $x: ${cpu[$x]}" # because I can't test pushbullets. you can remove this
+            # Save timestamp in seconds since Jan 1, 1970 to temp file
+            echo $(date +%s) > /tmp/last_cpu_temp_notify
+        fi
+    fi
 done
-
-
-if [[ $notify == "yes" ]]; then
-#  echo "$cpuFile" | mail -s "UNSAFE TEMPERATURE!" ${mmsAddr},${emailAddr}
-## Send Pushbullet notification
-#  ${pushbulletPath}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice "Unsafe Temperature!" "$cpuFile"
-  echo $cpuFile
-
-  rm /tmp/cpu
-  notify=""
-
-
-# wait 3 minutes if the temperature is above the "max" level so that user isn't bombarded with alerts. I may also define a "critical" level, which would then scale-back power to core x, but this would require another dependancy
-#sleep 180
-fi
-
-# wait 5 seconds between checking core temps
-#sleep 5
